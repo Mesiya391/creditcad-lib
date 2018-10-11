2b5a451289bc18856dea34debe03287213018a9e
Maria Pszczola <bjasinski391@gmail.com>
2018-10-11 10:12:55 +0000

    create card java env

---
+++ creditcard-app/src/main/java/pl/199830/creditcard/App.java
@@ -0,0 +1,13 @@
+package pl.199830.creditcard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
